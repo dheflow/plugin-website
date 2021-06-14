29418a21f881e79eaafc83d7763186e88aec9ffd
Tri Hariyanto <dheflow071@gmail.com>
2021-06-14 19:30:20 +0700

    first commit

---
+++ Footer.php
@@ -0,0 +1,62 @@
+<footer>
+  <div class="footer-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4 logo__section">
+          <img class="logo" src="images/your-logo.png" alt="logo" style="width: 100px;">
+          <p>Connect your cryptocurrency wallets and exchanges. Get your crypto and bitcoin taxes done in minutes.</p>
+        </div>
+        <div class="col-sm-2 footer-menu">
+          <h5>Platform</h5>
+          <ul>
+            <li><a href="#">Lorem ipsum</a></li>
+            <li><a href="#">Lorem ipsum</a></li>
+            <li><a href="#">Lorem ipsum</a></li>
+            <li><a href="#">Lorem ipsum</a></li>
+            <li><a href="#">Lorem ipsum</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-2 footer-menu">
+          <h5>Company</h5>
+          <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="FAQ.html">FAQ</a></li>
+            <li><a href="TOS.html">Terms Of Service</a></li>
+            <li><a href="Blog.html">Blog</a></li>
+            <li><a href="BlogPost.html">BlogPost</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-2 footer-menu">
+          <h5>Mobile</h5>
+          <ul>
+            <li><a href="#">Lorem ipsum</a></li>
+            <li><a href="#">Lorem ipsum</a></li>
+            <li><a href="#">Lorem ipsum</a></li>
+            <li><a href="#">Lorem ipsum</a></li>
+            <li><a href="#">Lorem ipsum</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-2 footer-menu">
+          <h5>Resources</h5>
+          <ul>
+            <li><a href="#">Lorem ipsum</a></li>
+            <li><a href="#">Lorem ipsum</a></li>
+            <li><a href="#">Lorem ipsum</a></li>
+            <li><a href="#">Lorem ipsum</a></li>
+            <li><a href="#">Lorem ipsum</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="footerBottomSection">
+        <p>© Themea 2021</p>
+        <div class="social-media">
+          <img class="my-auto" style="width: 32px; height: 32px; margin:5px;" src="images/facebook.svg">
+          <img class="my-auto" style="width: 32px; height: 32px; margin:5px;" src="images/reddit.svg">
+          <img class="my-auto" style="width: 32px; height: 32px; margin:5px;" src="images/telegram.svg">
+          <img class="my-auto" style="width: 32px; height: 32px; margin:5px;" src="images/youtube.svg">
+          <img class="my-auto" style="width: 32px; height: 32px; margin:5px;" src="images/twitter.svg">
+        </div>
+      </div>
+    </div>        
+  </div>      
+</footer>
---
+++ Navbar.php
@@ -0,0 +1,16 @@
+<div class="container">
+  <header>
+    <a href="/"><img class="logo" src="images/your-logo.png" alt="logo" style="width: 150px;"></a>
+    <nav>
+      <ul class="nav__links">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="FAQ.html">FAQ</a></li>
+        <li><a href="TOS.html">Terms of Service</a></li>
+        <li><a href="Blog.html">Blog</a></li>
+        <li><a href="#">Login</a></li>
+        <li><a href="#"><button class="unfillbtn">Get Started</button></a></li>
+      </ul>
+    </nav>
+  </header>
+</div>
+
